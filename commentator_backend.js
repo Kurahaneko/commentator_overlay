164e16319a530c615f2d91cb26f443b414a89d87
Kurahaneko <79398000+Kurahaneko@users.noreply.github.com>
2021-05-07 19:02:19 -0400

    Add files via upload

--- commentator_backend.js
+++ commentator_backend.js
@@ -30,7 +30,7 @@ function post(){
     img1_1.style.height = "100px"
     var defenders = document.getElementById("first_agent")
     defenders.appendChild(img1_1)
-    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_1' style='font-weight:bold; font-size:30px'>" + name1_1 + "</span>"
+    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_1' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name1_1 + "</span>"
     defenders_save[name1_1] = agent1_1
 
     var agent1_2 = $("#agent1-2 option:selected").text()
@@ -43,7 +43,7 @@ function post(){
     img1_2.style.height = "100px"
     var defenders = document.getElementById("second_agent")
     defenders.appendChild(img1_2)
-    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_2' style='font-weight:bold; font-size:30px'>" + name1_2 + "</span>"
+    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_2' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name1_2 + "</span>"
     defenders_save[name1_2] = agent1_2
 
     var agent1_3 = $("#agent1-3 option:selected").text()
@@ -56,7 +56,7 @@ function post(){
     img1_3.style.height = "100px"
     var defenders = document.getElementById("third_agent")
     defenders.appendChild(img1_3)
-    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_3' style='font-weight:bold; font-size:30px'>" + name1_3 + "</span>"
+    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_3' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name1_3 + "</span>"
     defenders_save[name1_3] = agent1_3
 
     var agent1_4 = $("#agent1-4 option:selected").text()
@@ -69,7 +69,7 @@ function post(){
     img1_4.style.height = "100px"
     var defenders = document.getElementById("fourth_agent")
     defenders.appendChild(img1_4)
-    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_4' style='font-weight:bold; font-size:30px'>" + name1_4 + "</span>"
+    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_4' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name1_4 + "</span>"
     defenders_save[name1_4] = agent1_4
 
     var agent1_5 = $("#agent1-5 option:selected").text()
@@ -82,7 +82,7 @@ function post(){
     img1_5.style.height = "100px"
     var defenders = document.getElementById("fifth_agent")
     defenders.appendChild(img1_5)
-    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_5' style='font-weight:bold; font-size:30px'>" + name1_5 + "</span>"
+    defenders.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent1_5' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name1_5 + "</span>"
     defenders_save[name1_5] = agent1_5
 
     var agent2_1 = $("#agent2-1 option:selected").text()
@@ -95,7 +95,7 @@ function post(){
     img2_1.style.height = "100px"
     var attackers = document.getElementById("sixth_agent")
     attackers.appendChild(img2_1)
-    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_1' style='font-weight:bold; font-size:30px'>" + name2_1 + "</span>"
+    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_1' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name2_1 + "</span>"
     attackers_save[name2_1] = agent2_1
 
     var agent2_2 = $("#agent2-2 option:selected").text()
@@ -108,7 +108,7 @@ function post(){
     img2_2.style.height = "100px"
     var attackers = document.getElementById("seventh_agent")
     attackers.appendChild(img2_2)
-    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_2' style='font-weight:bold; font-size:30px'>" + name2_2 + "</span>"
+    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_2' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name2_2 + "</span>"
     attackers_save[name2_2] = agent2_2
 
     var agent2_3 = $("#agent2-3 option:selected").text()
@@ -121,7 +121,7 @@ function post(){
     img2_3.style.height = "100px"
     var attackers = document.getElementById("eighth_agent")
     attackers.appendChild(img2_3)
-    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_3' style='font-weight:bold; font-size:30px'>" + name2_3 + "</span>"
+    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_3' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name2_3 + "</span>"
     attackers_save[name2_3] = agent2_3
 
     var agent2_4 = $("#agent2-4 option:selected").text()
@@ -134,7 +134,7 @@ function post(){
     img2_4.style.height = "100px"
     var attackers = document.getElementById("ninth_agent")
     attackers.appendChild(img2_4)
-    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_4' style='font-weight:bold; font-size:30px'>" + name2_4 + "</span>"
+    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_4' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name2_4 + "</span>"
     attackers_save[name2_4] = agent2_4
 
     var agent2_5 = $("#agent2-5 option:selected").text()
@@ -147,18 +147,18 @@ function post(){
     img2_5.style.height = "100px"
     var attackers = document.getElementById("tenth_agent")
     attackers.appendChild(img2_5)
-    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_5' style='font-weight:bold; font-size:30px'>" + name2_5 + "</span>"
+    attackers.innerHTML += "&nbsp;&nbsp;&nbsp; <span id='agent2_5' style='font-weight:bold; font-size:30px' contentEditable='true'>" + name2_5 + "</span>"
     attackers_save[name2_5] = agent2_5
 
     var attackers = document.getElementById("attackers1")
     var attackers_name = document.getElementById("attackers").value
     document.getElementById("attackers1").innerHTML = "";
-    attackers.innerHTML += "<span id='attacker-name' style='font-weight:bold; font-size:40px'>" + attackers_name + "</span>"
+    attackers.innerHTML += "<span id='attacker-name' style='font-weight:bold; font-size:40px' contentEditable='true'>" + attackers_name + "</span>"
 
     var defenders = document.getElementById("defenders1")
     var defenders_name = document.getElementById("defenders").value
     document.getElementById("defenders1").innerHTML = "";
-    defenders.innerHTML += "<span id='defender-name' style='font-weight:bold; font-size:40px'>" + defenders_name + "</span>"
+    defenders.innerHTML += "<span id='defender-name' style='font-weight:bold; font-size:40px' contentEditable='true'>" + defenders_name + "</span>"
 
     attackers_save["team_name"] = attackers_name
     defenders_save["team_name"] = defenders_name
